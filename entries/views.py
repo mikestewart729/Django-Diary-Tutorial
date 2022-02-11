87481dece1137f8ebd684f91814ecb87517a919b
Mike <mikestewart729@gmail.com>
2022-02-11 12:21:33 -0500

    Add login lock and fix incorrect routing

--- entries/views.py
+++ entries/views.py
@@ -1,5 +1,6 @@
 # entries/views.py
 
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
@@ -12,31 +13,35 @@ from django.views.generic import (
 )
 from .models import Entry
 
-class EntryListView(ListView):
+class LockedView(LoginRequiredMixin):
+    login_url = "admin:login"
+
+class EntryListView(LockedView, ListView):
     model = Entry
     queryset = Entry.objects.all().order_by("-date_created")
 
-class EntryDetailView(DetailView):
+class EntryDetailView(LockedView, DetailView):
     model = Entry
 
-class EntryCreateView(SuccessMessageMixin, CreateView):
+class EntryCreateView(LockedView, SuccessMessageMixin, CreateView):
     model = Entry
     fields = ["title", "content"]
     success_url = reverse_lazy("entry-list")
     success_message = "Your new entry was created."
 
-class EntryUpdateView(SuccessMessageMixin, UpdateView):
+class EntryUpdateView(LockedView, SuccessMessageMixin, UpdateView):
     model = Entry
     fields = ["title", "content"]
     success_message = "Your entry was updated."
 
     def get_success_url(self):
+        entry_id = self.kwargs['pk']
         return reverse_lazy(
             "entry-detail",
-            kwargs={"pk": self.entry.id}
+            kwargs={"pk": entry_id}
         )
 
-class EntryDeleteView(DeleteView):
+class EntryDeleteView(LockedView, DeleteView):
     model = Entry
     success_url = reverse_lazy("entry-list")
     success_message = "Your entry was deleted!"
