9d24c8fbb77ac6f6e3d4bc891c60db2221eef4a6
Mike <mikestewart729@gmail.com>
2022-02-11 11:20:54 -0500

    Update django diary functionality and appearance

--- entries/urls.py
+++ entries/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path("", views.EntryListView.as_view(), name="entry-list"),
     path("entry/<int:pk>", views.EntryDetailView.as_view(), name="entry-detail"),
+    path("create", views.CreateView.as_view(), name="entry-create"),
+    path("entry/<int:pk>/update", views.EntryUpdateView.as_view(), name="entry-update"),
+    path("entry/<int:pk>/delete", views.EntryDetailView.as_view(), name="entry-delete"),
 ]
